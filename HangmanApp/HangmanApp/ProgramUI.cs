929ef4a048461d518d01f961b0e6c4f5d98ba380
Otto Lutz <ottolutz00@gmail.com>
2021-08-19 11:42:17 -0400

    player now sees the category name they are currently playing, the user is now asked if they would like to start with a hint which would reveal a letter of the word

--- HangmanApp/HangmanApp/ProgramUI.cs
+++ HangmanApp/HangmanApp/ProgramUI.cs
@@ -10,10 +10,10 @@ namespace HangmanApp
     {
         //themed categories of words
         string[] city = { "london", "tokyo", "paris", "berlin", "washington", "budapest", "bangkok", "dubai", "singapore", "seoul", "barcelona", "osaka", "shanghai", "miami", "taipei", "istanbul" };
-        
+
         string[] animal = { "donkey", "goat", "horse", "cat", "sheep", "penguin", "kangaroo", "rabbit", "goldfish", "rhinoceros", "hedgehog", "gorilla", "leopard", "chimpanzee", "impala", "hippopotamus" };
 
-        string[] foodAndDrinks = { "spaghetti", "pizza", "salad", "fanta", "hamburger", "steak", "pretzel", "water", "juice", "sausage", "yogurt", "coffee", "omelet", "kebadb", "breadsticks","croissant" };
+        string[] foodAndDrinks = { "spaghetti", "pizza", "salad", "fanta", "hamburger", "steak", "pretzel", "water", "juice", "sausage", "yogurt", "coffee", "omelet", "kebadb", "breadsticks",  "croissant" };
 
 
         public void Run()
@@ -25,6 +25,10 @@ namespace HangmanApp
         //stores every character the player has already guessed for the current word, resets every round
         List<char> guessedLetters = new List<char>();
         int guessesLeft;
+        //if the player wants a hint this will be true and didplayword will randomly show a letter form the word
+        bool playerWantsHint = false;
+        //category name stored for displaycategory
+        string category;
         //win stats
         double numberOfGamesPlayed = 0;
         double numberOfGamesWon = 0;
@@ -48,6 +52,9 @@ namespace HangmanApp
                 Console.Write("How many guesses would you like to complete the word: ");
                 string stringInput = Console.ReadLine();
                 guessesLeft = Convert.ToInt32(stringInput);
+
+                AskUserForHint();
+
                 AssignAndRunNumberOfTurns(Convert.ToInt32(stringInput));
 
                 //this will run at the end of each round, clearing the list of prevoiusly guessed characters and number of guesses left
@@ -56,15 +63,15 @@ namespace HangmanApp
             }
         }
 
-  
+
         //FEATURES TO ADD
-        
-        
-        //show the player what letters have already been guessed
+        //display the current category the user is in, (CITIES, ANIMALS, FOOD & DRINK)
 
         //(do this last if we have time)start the round with one letter already revealed to help the user make their first guess. Ask the user if they would like to "start the round with a hint?"
 
-        
+
+        // and if the word is "museum" and 'm' is chosen it needs to reveal both and not just one 'm', just something to watch out for.
+
 
         //METHODS
 
@@ -73,6 +80,7 @@ namespace HangmanApp
         {
 
             Console.Clear();
+            DisplayCategory();
             DisplayWord();
             DisplayNumberOfGuessesLeft();
             DisplayGuessedLetters();
@@ -87,6 +95,11 @@ namespace HangmanApp
                 ContinueMessage();
                 GetGuess();
             }
+        }
+
+        public void DisplayCategory()
+        {
+            Console.WriteLine("Category: " + category);
         }
 
         //CheckIfWordIsGuesses - returns true if every letter of the word has been guessed, returns false if the player has not guessed every letter in the word.
@@ -106,8 +119,9 @@ namespace HangmanApp
         //DisplayWord - prints a " _ " for letters of the word that have not been guessed, and if the player has guessed the letter it will print that letter
         public void DisplayWord()
         {
+
             Console.Write("\n  ");
-            
+
             foreach (char wordLetter in word)
             {
                 if (guessedLetters.Contains(wordLetter))
@@ -152,14 +166,17 @@ namespace HangmanApp
             {
                 case 1:
                     int index = rand.Next(0, city.Length);
+                    category = "cities";
                     return city[index];
                     break;
                 case 2:
                     int indexx = rand.Next(0, animal.Length);
+                    category = "animals";
                     return animal[indexx];
                     break;
                 case 3:
                     int indexxx = rand.Next(0, foodAndDrinks.Length);
+                    category = "food & drink";
                     return foodAndDrinks[indexxx];
                     break;
                 default:
@@ -167,11 +184,42 @@ namespace HangmanApp
                     return city[indexxxx];
                     break;
             }
+            
+        }
+        //im going to test now
+        //just figured out that i can't put that switch after the other one because it wont run, i will make a method to contain the stuff
+              
+        //asks the user if they want a hint, if yes then the displayword method will reveal the a random letter from word.
+        public void AskUserForHint()
+        {
+            Console.Write("Would you like to start the round with a hint? (y/n): ");
+            string hintInput = Console.ReadLine();
+            switch (hintInput)
+            {
+                case "y":
+                case "ye":
+                case "yes":
+                    AddHintedLetterToGuessedLetters();
+                    break;
+                case "n":
+                case "no":
+                    break;
+                default:
+                    break;
+            }
+            ContinueMessage();
         }
 
+        //runs inside the AskUserForHint method if the user accepts the hint, the method will then take a random letter from word and add it to the list of guessed letters
+        public void AddHintedLetterToGuessedLetters()
+        {
+            Random rand = new Random();
+            string str = word.Substring(rand.Next(0, word.Length + 1), 1);
+            char character = Convert.ToChar(str);
+            guessedLetters.Add(character);
+            
+        }
         
-
-
         //prints the win rate as a percentage, also prints the number of wins, and number of games played
         public void DisplayWinPercentage()
         {
